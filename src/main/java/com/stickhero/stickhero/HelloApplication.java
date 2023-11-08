4b2712935f48fee16492d2b0f751d305850cb7d2
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-11-08 15:52:34 +0530

    Changes

--- src/main/java/com/stickhero/stickhero/HelloApplication.java
+++ src/main/java/com/stickhero/stickhero/HelloApplication.java
@@ -3,14 +3,29 @@ package com.stickhero.stickhero;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        HBox hbox = new HBox();
+        Scene scene = new Scene(hbox, 800, 600);
 
+        Image image = new Image(this.getClass().getResourceAsStream("Pic_1.jpg"));
+
+        BackgroundImage backgroundimage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+
+        Background background = new Background(backgroundimage);
+
+        hbox.setBackground(background);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
