8ad44705afeb5c92560d56ed68c2f36d7cfc0257
het <het.shah25052004@gmail.com>
2023-11-11 00:27:24 +0530

    changed Background_Selector

--- src/main/java/com/stickhero/stickhero/Background_Selector.java
+++ src/main/java/com/stickhero/stickhero/Background_Selector.java
@@ -12,35 +12,14 @@ public class Background_Selector {
     public void setImagesArray(ArrayList<String> imagesArray){
         this.imagesArray = imagesArray;
     }
-
-    public ArrayList<String> loadImages(){
-        ArrayList<String> tempArray = new ArrayList<>();
-        tempArray.add("Pic_1.jpg");
-        tempArray.add("Pic_2.jpg");
-        tempArray.add("Pic_3.jpg");
-        tempArray.add("Pic_4.jpg");
-        tempArray.add("Pic_5.jpg");
-        tempArray.add("Pic_6.jpg");
-        tempArray.add("Pic_7.jpg");
-        tempArray.add("Pic_8.jpg");
-        tempArray.add("Pic_9.jpg");
-        tempArray.add("Pic_10.jpg");
-        tempArray.add("Pic_11.jpg");
-        tempArray.add("Pic_12.jpg");
-
-        return tempArray;
-    }
-
-    public String selectRandomImage(ArrayList<String> imagesArray){
-        int rand = (int)(Math.random() * imagesArray.size());
-        return imagesArray.get(rand);
+    public String selectRandomImage(){
+        int rand = (int)(Math.random() * 12) + 1;
+        String num = Integer.toString(rand);
+        return num;
     }
     public HBox selectOne(){
-        setImagesArray(loadImages());
-
-//        Image image = new Image(this.getClass().getResourceAsStream("Pic_3.jpg"));
-
-        Image image = new Image(this.getClass().getResourceAsStream(selectRandomImage(imagesArray)));
+        String randomImage = "Pic_" + selectRandomImage() + ".jpg";
+        Image image = new Image(getClass().getResourceAsStream(randomImage));
 
         BackgroundSize backgroundSize = new BackgroundSize(1000,650,false,false,true,true);
         BackgroundImage backgroundimage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
