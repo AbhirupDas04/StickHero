9443d9adb8424f6219e017a890d1e0a0ae981562
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-11-15 18:16:57 +0530

    Changes

--- src/main/java/com/stickhero/stickhero/EndScreen.java
+++ src/main/java/com/stickhero/stickhero/EndScreen.java
@@ -1,6 +1,8 @@
 package com.stickhero.stickhero;
 
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class EndScreen extends BackgroundHandler {
@@ -14,6 +16,13 @@ public class EndScreen extends BackgroundHandler {
         this.image = image;
     }
     public void endGame(){
+        BackgroundSize backgroundSize = new BackgroundSize(1000,650,false,false,true,true);
+        BackgroundImage backgroundimage = new BackgroundImage(this.image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
+        this.setBackground(new Background(backgroundimage));
 
+        Pane pane = this.returnBackground();
+        Scene scene1 = new Scene(pane, 500, 650);
+        super.getStage().setScene(scene1);
+        super.getStage().show();
     }
 }
