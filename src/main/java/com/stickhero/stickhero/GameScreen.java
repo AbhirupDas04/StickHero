ce282bdfb1dc10caa92e20b6418bbf93da0e06c4
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-11-26 23:28:29 +0530

    Changes

--- src/main/java/com/stickhero/stickhero/GameScreen.java
+++ src/main/java/com/stickhero/stickhero/GameScreen.java
@@ -32,6 +32,10 @@ public class GameScreen extends BackgroundHandler {
     private Rectangle pillar2_rect;
     private Pillar pillar1;
     private Pillar pillar2;
+    private int curr_pillar_width;
+    private ImageView view;
+    private Rectangle first_red_bar;
+    private Rectangle old_stick;
     public GameScreen(Stage stage, HistoryStorage storage){
         super(stage);
         this.hero = new Hero();
@@ -67,6 +71,7 @@ public class GameScreen extends BackgroundHandler {
         Pillar pillar = new Pillar(100,160,0,490);
         Rectangle rectangle = pillar.getRectangle();
         this.pillar1_rect = rectangle;
+        this.curr_pillar_width = 100;
         this.pillar1 = pillar;
 
         Random rand = new Random();
@@ -83,7 +88,7 @@ public class GameScreen extends BackgroundHandler {
         rect3.setLayoutX(rand_posX + rand_width / 2.0 - 5);
         rect3.setLayoutY(490);
 
-        ImageView view = hero.generateStandingHero(35,35,55,455);
+        view = hero.generateStandingHero(35,35,55,455);
 
         Rectangle rectangle1 = new Rectangle(70, 40);
         rectangle1.setFill(Color.GRAY);
@@ -145,7 +150,8 @@ public class GameScreen extends BackgroundHandler {
     }
 
     public void playGame(int next_pillar_start,int next_pillar_width, Rectangle red_bar){
-        Stick stick = new Stick(3, 95, 480);
+        GameScreen game = this;
+        Stick stick = new Stick(3, curr_pillar_width - 5, 490);
         Rectangle rectangle4 = stick.generateStick();
 
         Pane pane = this.getPane();
@@ -154,14 +160,19 @@ public class GameScreen extends BackgroundHandler {
         int rand_width = rand.nextInt(70) + 30;
         int rand_posX = rand.nextInt(370 - (next_pillar_width + 30)) + next_pillar_start + next_pillar_width + 30;
         int act_rand_posX = rand_posX;
-        if(rand_posX <= 500){
+        int gap = act_rand_posX - 30 - next_pillar_start - next_pillar_width;
+        if (rand_posX <= 500) {
             rand_posX = 500;
         }
-        Pillar pillar3 = new Pillar(rand_width,160,rand_posX,490);
+        Pillar pillar3 = new Pillar(rand_width, 160, rand_posX, 490);
         Rectangle rectangle2 = pillar3.getRectangle();
-        pane.getChildren().addAll(rectangle2,rectangle4);
 
-        ImageView view = this.hero.getView();
+        Rectangle rect3 = new Rectangle(10, 5);
+        rect3.setFill(Color.RED);
+        rect3.setLayoutX(rand_posX + rand_width / 2.0 - 5);
+        rect3.setLayoutY(490);
+
+        pane.getChildren().addAll(rectangle2, rectangle4, rect3);
 
         Timeline timeline = new Timeline();
 
@@ -174,46 +185,87 @@ public class GameScreen extends BackgroundHandler {
         AnimationTimer timer1 = new AnimationTimer() {
             int deg = 0;
             boolean flag = false;
+
             @Override
             public void handle(long l) {
-                if(flag == false) {
+                if (flag == false) {
                     rectangle4.getTransforms().add(new Rotate(1, 3, stick.getHeight()));
                     deg++;
                     if (deg == 90) {
                         stop();
-                        flag = true;
-                        Image image1 = new Image(this.getClass().getResourceAsStream("Standing_Hero.png"));
-                        Image image2 = new Image(this.getClass().getResourceAsStream("Side_On.png"));
-                        for(int i = 0; i < stick.getHeight() + 20; i++){
-                            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + i*10), new KeyValue (view.translateXProperty(), 1 + i*1)));
-                            if(i%20 == 0){
-                                timeline.getKeyFrames().add(new KeyFrame(Duration.millis(5 + i*10), new KeyValue(view.imageProperty(),image1)));
-                            }
-                            else if (i%20 == 10){
-                                timeline.getKeyFrames().add(new KeyFrame(Duration.millis(5 + i*10), new KeyValue(view.imageProperty(),image2)));
-                            }
-                        }
-                        int temp_cur = stick.getHeight() + 20;
-                        for(int j = stick.getHeight() + 20; j < stick.getHeight() + 20 + pillar2.getX_pos(); j++){
-                            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j *10), new KeyValue (pillar2_rect.translateXProperty(), -(1+(j-stick.getHeight()- 20)))));
-                            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j *10), new KeyValue (pillar1_rect.translateXProperty(), -(1+(j-stick.getHeight()- 20)))));
-                            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j *10), new KeyValue (rectangle4.translateXProperty(), -(1+(j-stick.getHeight()- 20)))));
-                            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j *10), new KeyValue (red_bar.translateXProperty(), -(1+(j-stick.getHeight()- 20)))));
-                            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j *10), new KeyValue (rectangle2.translateXProperty(), -(1+(j-stick.getHeight()- 20)))));
-                            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j *10), new KeyValue (view.translateXProperty(), temp_cur)));
-                            temp_cur-=1;
-                        }
-                        timeline.play();
+                        fn();
                     }
                 }
+            }
+
+            public void fn(){
+                timeline.setOnFinished(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        curr_pillar_width = next_pillar_width;
+                        pillar1 = pillar2;
+                        pillar1_rect = pillar2_rect;
+                        pillar1_rect.setTranslateX(0);
+                        pillar1_rect.setLayoutX(0);
+                        pillar1.setX_pos(0);
+                        pillar2 = pillar3;
+                        pillar2_rect = rectangle2;
+                        pillar2_rect.setTranslateX(0);
+                        pillar2_rect.setLayoutX(gap + 30 + pillar1.getWidth());
+                        pillar2.setX_pos(gap + 30 + pillar1.getWidth());
+                        rect3.setTranslateX(0);
+                        rect3.setLayoutX(gap + 30 + pillar1.getWidth() + pillar3.getWidth()/2.0 - 5);
+                        first_red_bar = red_bar;
+                        first_red_bar.setTranslateX(0);
+                        first_red_bar.setLayoutX(pillar1.getWidth()/2.0 - 5);
+                        view.setTranslateX(0);
+                        view.setLayoutX(pillar1.getWidth() - 43);
+                        old_stick = rectangle4;
+                        game.playGame(gap + 30 + pillar1.getWidth(),pillar3.getWidth(),rect3);
+                    }
+                });
+
+                flag = true;
+                Image image1 = new Image(this.getClass().getResourceAsStream("Standing_Hero.png"));
+                Image image2 = new Image(this.getClass().getResourceAsStream("Side_On.png"));
+                int distance =  (pillar2.getX_pos() + pillar2.getWidth() - 45 - (int)view.getLayoutX());
+                for (int i = 0; i < distance; i++) {
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + i * 10), new KeyValue(view.translateXProperty(), 1 + i * 1)));
+                    if (i % 20 == 0) {
+                        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(5 + i * 10), new KeyValue(view.imageProperty(), image1)));
+                    } else if (i % 20 == 10) {
+                        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(5 + i * 10), new KeyValue(view.imageProperty(), image2)));
+                    }
+                }
+                int temp_cur = distance;
+                for (int j = distance; j < distance + pillar2.getX_pos(); j++) {
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j * 10), new KeyValue(pillar2_rect.translateXProperty(), -(1 + (j - distance)))));
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j * 10), new KeyValue(pillar1_rect.translateXProperty(), -(1 + (j - distance)))));
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j * 10), new KeyValue(rectangle4.translateXProperty(), -(1 + (j - distance)))));
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j * 10), new KeyValue(red_bar.translateXProperty(), -(1 + (j - distance)))));
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j * 10), new KeyValue(view.translateXProperty(), temp_cur)));
+                    if(first_red_bar != null){
+                        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + j * 10), new KeyValue(first_red_bar.translateXProperty(), -(1 + (j - distance)))));
+                    }
+                    if(old_stick != null){
+                        old_stick.setVisible(false);
+                    }
+                    temp_cur -= 1;
+                }
+                for (int k = distance; k < distance + pillar3.getX_pos() - (pillar2.getWidth() + gap + 30); k++) {
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + k * 10), new KeyValue(rectangle2.translateXProperty(), -(1 + (k - distance)))));
+                    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10 + k * 10), new KeyValue(rect3.translateXProperty(), -(1 + (k - distance)))));
+                }
+                timeline.play();
             }
         };
 
         this.getScene().setOnMousePressed(new EventHandler<MouseEvent>() {
             boolean flag = false;
+
             @Override
             public void handle(MouseEvent mouseEvent) {
-                if(!flag){
+                if (!flag) {
                     timer.start();
                     flag = true;
                 }
--- src/main/java/com/stickhero/stickhero/Stick.java
+++ src/main/java/com/stickhero/stickhero/Stick.java
@@ -11,13 +11,13 @@ public class Stick {
     private int height;
 
     public Stick(int width, int x_pos, int y_pos){
-        Rectangle rectangle = new Rectangle(width,10);
+        Rectangle rectangle = new Rectangle(width,0);
         rectangle.setLayoutX(x_pos);
         rectangle.setLayoutY(y_pos);
         this.rectangle = rectangle;
         this.speed = 1;
-        this.length = 10;
-        this.height = 10;
+        this.length = 0;
+        this.height = 0;
         this.y_pos = y_pos;
         this.x_pos = x_pos;
     }
