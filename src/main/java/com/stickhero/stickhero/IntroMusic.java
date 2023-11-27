ac825569ef7e7d022738c7f1a3da1c7e397e79c5
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-11-27 17:27:01 +0530

    Changes

--- src/main/java/com/stickhero/stickhero/IntroMusic.java
+++ src/main/java/com/stickhero/stickhero/IntroMusic.java
@@ -1,4 +1,48 @@
 package com.stickhero.stickhero;
 
-public class IntroMusic extends MusicHandler {
+//public class InGameMusic extends MusicHandler {
+//}
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+public class IntroMusic extends MusicHandler
+{
+    private MediaPlayer mediaPlayer;
+    public void start (Stage primaryStage) throws Exception {
+        String musicPath = "src/main/resources/com/stickhero/stickhero/entry_page_music.mp3";
+
+        Media media = new Media(new File(musicPath).toURI().toString());
+
+        mediaPlayer = new MediaPlayer(media);
+
+//        MediaView mediaView = new MediaView(mediaPlayer);
+//        mediaView.setMediaPlayer(mediaPlayer);
+
+        mediaPlayer.setAutoPlay(true);
+
+//        primaryStage.setTitle("Playing Audio");
+        primaryStage.show();
+        mediaPlayer.setOnEndOfMedia(() -> {
+            // Repeat the music when it reaches the end
+            mediaPlayer.seek(Duration.ZERO);
+        });
+
+        mediaPlayer.play();
+    }
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+
 }
