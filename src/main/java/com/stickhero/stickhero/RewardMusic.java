99422bcef0bcd662477c4a6942d67a841ef0eb55
hetshah <het22213@iiitd.ac.in>
2023-12-09 22:59:44 +0530

    changes

---
+++ src/main/java/com/stickhero/stickhero/RewardMusic.java
@@ -0,0 +1,45 @@
+/*
+ * THIS CLASS HANDLES THE WORKING OF STICK FALLING MUSIC
+ * */
+
+
+package com.stickhero.stickhero;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class RewardMusic extends MusicHandler
+{
+    private MediaPlayer mediaPlayer;
+
+    public void setMediaPlayer(MediaPlayer mediaPlayer){
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    public MediaPlayer getMediaPlayer(){
+        return this.mediaPlayer;
+    }
+
+    public void start (Stage primaryStage) {
+        Media media = new Media(getClass().getResource("reward_music.mp3").toString());
+
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+
+        primaryStage.show();
+        mediaPlayer.setOnEndOfMedia(() -> {
+            mediaPlayer.seek(Duration.ZERO);
+        });
+
+        mediaPlayer.play();
+    }
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+
+}
